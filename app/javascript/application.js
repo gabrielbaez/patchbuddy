ab3610285074afaa5ce6fb581fe3b55e4348cd5a
Gabriel Baez <gabrielbaez@bohiti.org>
2023-07-03 21:05:34 -0500

    - initial with working devise setup using email

---
+++ app/javascript/application.js
@@ -0,0 +1,18 @@
+// Entry point for the build script in your package.json
+import "@hotwired/turbo-rails"
+import "./controllers"
+
+import * as bootstrap from "bootstrap"
+
+import "./modules/sidebar"
+import "./modules/theme"
+import "./modules/feather"
+
+// Charts
+import "./modules/chartjs"
+
+// Forms
+import "./modules/flatpickr"
+
+// Maps
+import "./modules/vector-maps"
